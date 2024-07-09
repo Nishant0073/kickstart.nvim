1b5c06f8ef6ada90892db2581e01df94485e3494
Nishant Shingate <nishantks121@gmail.com>
2024-07-09 12:18:11 +0530

    some new plugins and changes

--- init.lua
+++ init.lua
@@ -396,7 +396,7 @@ require('lazy').setup({
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
-          previewer = false,
+          previewer = true,
         })
       end, { desc = '[/] Fuzzily search in current buffer' })
 
@@ -779,7 +779,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<TAB>'] = cmp.mapping.confirm { select = true },
+          -- ['<TAB>'] = cmp.mapping.confirm { select = true },
           ['<CR>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
@@ -825,21 +825,83 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
+  {
+    'sekke276/dark_flat.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('dark_flat').setup {
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+        cursorline = true,
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = 'dark', -- style for sidebars, see below
+          floats = 'dark', -- style for floating windows
+        },
+        sidebars = { 'qf', 'help' }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+      }
+    end,
+    -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- 'folke/tokyonight.nvim',
+    -- priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- config = function()
+    --   require('tokyonight').setup {
+    --     -- your configuration comes here
+    --     -- or leave it empty to use the default settings
+    --     style = 'storm', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+    --     light_style = 'day', -- The theme is used when the background is set to light
+    --     transparent = false, -- Enable this to disable setting the background color
+    --     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+    --     styles = {
+    --       -- Style to be applied to different syntax groups
+    --       -- Value is any valid attr-list value for `:help nvim_set_hl`
+    --       comments = { italic = true },
+    --       keywords = { italic = true },
+    --       functions = {},
+    --       variables = {},
+    --       -- Background styles. Can be "dark", "transparent" or "normal"
+    --       sidebars = 'dark', -- style for sidebars, see below
+    --       floats = 'dark', -- style for floating windows
+    --     },
+    --     sidebars = { 'qf', 'help' }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+    --     day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+    --     hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+    --     dim_inactive = false, -- dims inactive windows
+    --     lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+    --
+    --     --- You can override specific color groups to use other groups or a hex color
+    --     --- function will be called with a ColorScheme table
+    --     ---@param colors ColorScheme
+    --     on_colors = function(colors) end,
+    --
+    --     --- You can override specific highlights to use other groups or a hex color
+    --     --- function will be called with a Highlights and ColorScheme table
+    --     ---@param highlights Highlights
+    --     ---@param colors ColorScheme
+    --     on_highlights = function(highlights, colors) end,
+    --   }
+    -- end,
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      --   -- Load the colorscheme here.
+      --   -- Like many other themes, this one has different styles, and you could load
+      --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      --   -- vim.cmd.colorscheme 'embark'
+      vim.cmd.colorscheme 'dark_flat'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 
@@ -960,5 +1022,10 @@ require('lazy').setup({
   },
 })
 
+vim.opt.termguicolors = true
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
+vim.diagnostic.config {
+  virtual_text = false,
+}
